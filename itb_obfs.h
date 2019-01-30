d2305c9c7a4ea7566b5cf4b72d922aed536151c5
Isaac Morneau <isaacmorneau@gmail.com>
2019-01-30 12:51:00 -0800

    moving obfuscation to a different repo, it doesnt fit here

--- itb_obfs.h
+++
@@ -1,6 +0,0 @@
-//this is a demo for generating encrypted strings easily
-#define SOME_STRING "this is the most straightforward example"
-
-//it leaves formatting the same, this is designed to be included with the build system
-#define OTHER_CONSTANT "does this parse? \167\157\162\153\163 \u263A\n"
-
